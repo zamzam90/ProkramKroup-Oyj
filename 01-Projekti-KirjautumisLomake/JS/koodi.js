805a953656baf20cd3244d5fc7ecd953298de6d7
zamzam90 <siltanen.sami@hotmail.com>
2021-03-25 12:55:26 +0200

    en osaa DOM / Sami / 25032021

--- 01-Projekti-KirjautumisLomake/JS/koodi.js
+++ 01-Projekti-KirjautumisLomake/JS/koodi.js
@@ -1,28 +1,5 @@
 //Kirjautumislomake
 console.info("koodi.js loaded..");//for debugging
-//funktio joka tarkastaa lomakkeen:
-function checkLoginForm() {
-//alustetaan muuttujia..
-//nimi & salasana:
-var userName = document.getElementById("user").value;
-console.log(userName);//for debugging..
-var password = document.getElementById("password").value;
-//radionapit:
-var radioNappi1 = document.getElementById("radio1").value;
-var radioNappi2 = document.getElementById("radio2").value;
-var radioNappi3 = document.getElementById("radio3").value;
-//checkboxit:
-var zekkiNappi1 = document.getElementById("check1").value;
-var zekkiNappi2 = document.getElementById("check2").value;
-var zekkiNappi3 = document.getElementById("check3").value;
-//dropdowni:
-var dropdowni = document.getElementById("dropdown").value;
-//kommenttikenttä:
-var kommenttikenttä = document.getElementById("feedback").value;
-//
-if (userName.length < 4) {
-  alert("nimi alle 4 kirjainta");
-} else {
-  alert("nimi yli 4 kirjainta");
-}
+function checkLoginForm(form) {
+  alert("test");
 }
