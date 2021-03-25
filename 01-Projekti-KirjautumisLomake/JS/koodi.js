f045c76d4e2f00f1210c4017f9f53bfc5d5a494a
zamzam90 <siltanen.sami@hotmail.com>
2021-03-25 13:44:09 +0200

    etenee hitaasti / Sami / 25032021

--- 01-Projekti-KirjautumisLomake/JS/koodi.js
+++ 01-Projekti-KirjautumisLomake/JS/koodi.js
@@ -1,14 +1,17 @@
 //Kirjautumislomake
-console.info("koodi.js loaded..");//for debugging
+console.info("koodi.js loaded.."); //for debugging
 function checkLoginForm(form) {
   //alustetaan muuttujia, haetaan niihin tiedot lomakkeesta:
   var userName = form[0].name.value;
   //tarkistetaan nimen pituus:
   if (userName.length < 3) {
-    console.log("Nimen pituus: " + userName.length);//for debugging
+    console.log("Nimen pituus: " + userName.length); //for debugging
     alert("Nimi alle 3 kirjainta!");
     return false;
   }
   //tarkistetaan salasanan pituus (pitää olla yli 15 merkkiä), mitä pidempi sen parempi :D
-
+  if (passWord.length < 15) {
+    alert("Liian lyhyt salasana!");
+    return false;
+  }
 }
