11f038ba8efd546adef258a67b8f482d188cdee5
zamzam90 <siltanen.sami@hotmail.com>
2021-03-25 14:04:52 +0200

    pikkuedit.. / Sami / 25032021

--- 01-Projekti-KirjautumisLomake/JS/koodi.js
+++ 01-Projekti-KirjautumisLomake/JS/koodi.js
@@ -2,7 +2,7 @@
 console.info("koodi.js loaded.."); //for debugging
 function checkLoginForm(form) {
   //alustetaan muuttujia, haetaan niihin tiedot lomakkeesta:
-  var userName = form[0].name.value;
+  var userName = form.name.value;
   //tarkistetaan nimen pituus:
   if (userName.length < 3) {
     console.log("Nimen pituus: " + userName.length); //for debugging
