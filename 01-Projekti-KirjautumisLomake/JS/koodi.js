5dd1c507f863f3c120eb16a165fc774d4088b7b7
zamzam90 <siltanen.sami@hotmail.com>
2021-03-25 13:02:44 +0200

    JS DOM on ihanaa / Sami / 25032021

--- 01-Projekti-KirjautumisLomake/JS/koodi.js
+++ 01-Projekti-KirjautumisLomake/JS/koodi.js
@@ -1,5 +1,9 @@
 //Kirjautumislomake
 console.info("koodi.js loaded..");//for debugging
 function checkLoginForm(form) {
-  alert("test");
+  var userName = form.name.value;
+  if (userName.length < 5) {
+    alert("Nimi alle 5 kirjainta!");
+    return false;
+  }
 }
