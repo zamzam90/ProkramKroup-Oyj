5ef9f7e8d627d7e908febd97e57cb94ec8198bf9
JohPii <johanna.piiroinen@edu.keuda.fi>
2021-03-31 14:00:50 +0300

    isosti muokkausta, ei toiminu mikään ed.lisäys, nyt toimii taas perusjutut eli nimi, sposti, ikä / Johanna / 31032021

--- 01B-Projekti-KirjautumisLomake2/JS/koodi.js
+++ 01B-Projekti-KirjautumisLomake2/JS/koodi.js
@@ -6,15 +6,15 @@ function kyselylomake() {
 var annaNimi = document.getElementById('nimi').value;
 var sahkoposti = document.getElementById('email').value;
 var x = document.getElementById('ika').value;
-var henkinenIka = dropdownlist.options[dropdownlist.selectedIndex].value;
-var raaadiot = document.getElementById('radior1');
-var kayttojarjestelma = false;
-var radionappulat = document.getElementById('radior2');
-var atk = false;
-var tsekkibox = document.getElementsByName('tsekbox');
-var taitotaso = false;
-var dropdownia = document.getElementById('dropdown');
-var kommentti = document.getElementById('name').value;
+//var henkinenIka = dropdownlist.options[dropdownlist.selectedIndex].value;
+//var raaadiot = document.getElementById('radior1');
+//var kayttojarjestelma = false;
+//var radionappulat = document.getElementById('radior2');
+//var atk = false;
+//var tsekkibox = document.getElementsByName('tsekbox');
+//var taitotaso = false;
+//var dropdownia = document.getElementById('dropdown');
+//var kommentti = document.getElementById('name').value;
 
 
 console.log(annaNimi.length);
@@ -27,11 +27,13 @@ if (annaNimi.length < 3) {
 //Söhköpostin tarkastus
 if(emailIsValid(sahkoposti)) {
 }
-  else {
+else {
   alert("Anna oikea sähköpostiosoitteesi!");
   return false;
 }
-
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
 //Ikä-kentän numero tarkastus
 //iän on oltava numero, määritetään se isNaN:Lla, muutoin perus else if:t
 
@@ -47,8 +49,10 @@ else if (x>100) {
   alert("Olet jo aika iäkäs, otahan lepoa vaan!");
   return false;
 }
-return true;
+  return true;
 
+
+/*
 //tarkistus että dropdown valikosta on valittu jotain:
 if (henkinenIka == "empty") {
   alert("Valitse henkinen ikäsi.");
@@ -92,4 +96,5 @@ if (kommentti.length < 10) {
   alert("Anna kommentteihin vähintää 10 merkkiä!")
   return false;
 }
-}
+}*/
+ }
