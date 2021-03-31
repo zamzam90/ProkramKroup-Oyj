604991b373dc367395be837fcdd094192c6db487
JohPii <johanna.piiroinen@edu.keuda.fi>
2021-03-31 11:05:29 +0300

    radio, check, dropdown ym härpäkkeet testiin / Johanna / 31032021

--- 01B-Projekti-KirjautumisLomake2/JS/koodi.js
+++ 01B-Projekti-KirjautumisLomake2/JS/koodi.js
@@ -6,6 +6,16 @@ function kyselylomake() {
 var annaNimi = document.getElementById('nimi').value;
 var sahkoposti = document.getElementById('email').value;
 var x = document.getElementById('ika').value;
+var henkinenIka = dropdownlist.options[dropdownlist.selectedIndex].value;
+var raaadiot = document.getElementById('radior1');
+var kayttojarjestelma = false;
+var radionappulat = document.getElementById('radior2');
+var atk = false;
+var tsekkibox = document.getElementsByName('tsekbox');
+var taitotaso = false;
+var dropdownia = document.getElementById('dropdown');
+var kommentti = document.getElementById('name').value;
+
 
 console.log(annaNimi.length);
 //Käyttäjänimen pituuden tarkistus
@@ -15,20 +25,15 @@ if (annaNimi.length < 3) {
 }
 
 //Söhköpostin tarkastus
-
-function emailIsValid (email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) }
-
-if(emailIsValid(sahkoposti)) { //
- }
- else {
+if(emailIsValid(sahkoposti)) {
+}
+  else {
   alert("Anna oikea sähköpostiosoitteesi!");
-  lomake.email.focus();
   return false;
 }
 
 //Ikä-kentän numero tarkastus
-//iän on oltava numero, määritetään se isNaN:Lla
+//iän on oltava numero, määritetään se isNaN:Lla, muutoin perus else if:t
 
 if (isNaN(x)) {
   alert("Iän on oltava numeroita!");
@@ -43,4 +48,48 @@ else if (x>100) {
   return false;
 }
 return true;
+
+//tarkistus että dropdown valikosta on valittu jotain:
+if (henkinenIka == "empty") {
+  alert("Valitse henkinen ikäsi.");
+  return false;
+}
+
+//ekoille radiobuttoneille haku, että on jotain valittuna
+for (var x = 0; x < raaadiot.length; x++) {
+  if (raaadiot[x].checked == true) {
+    kayttojarjestelma = true;
+  }
+}
+if (kayttojarjestelma == false) {
+  alert("Valitse mitä käyttöjärjestelmää käytät.");
+  return false;
+}
+
+//tokille radiobuttoneille haku, että on jotain valittuna
+for (var x = 0; x < radionappulat.length; x++) {
+  if (radionappulat[x].checked == true) {
+    atk = true;
+  }
+}
+if (atk == false) {
+  alert("Valitse käyttämäsi ATK.");
+  return false;
+}
+
+//checkboxeille haku, että on valittuna
+for (var y = 0; y < tsekkibox.length; y++) {
+  if (tsekkibox[y].checked == true) {
+    taitotaso = true;
+  }
+}
+if (taitotaso == false) {
+  alert("Valitse CS:GO taitotasosi!");
+  return false;
+}
+//Kommenttikentän tsekkaus
+if (kommentti.length < 10) {
+  alert("Anna kommentteihin vähintää 10 merkkiä!")
+  return false;
+}
 }
