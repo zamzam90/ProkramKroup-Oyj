67dff675c95ebac78930f53f4937019ae303c270
zamzam90 <siltanen.sami@hotmail.com>
2021-04-01 09:34:23 +0300

    lisätty tyhjän kentän tarkistus / Sami / 01042021

--- 01-Projekti-KirjautumisLomake/JS/koodi.js
+++ 01-Projekti-KirjautumisLomake/JS/koodi.js
@@ -2,21 +2,27 @@
 //console.info("koodi.js loaded.."); //for debugging
 function checkLoginForm(form) {
   //alustetaan muuttujia, haetaan niihin tiedot lomakkeesta:
-  var käyttäjäNimi = form.name.value;
-  var salasana = form.password.value;
+  var käyttäjäNimi = document.getElementById("user").value;
+  var salasana = document.getElementById("password").value;
   var radiobuttonit = document.getElementsByName("radio");
   var millainenKoneenKäyttäjä = false;
   var checkboksit = document.getElementsByName("box");
   var mitäMieltäSivusta = false;
   var palauteboxi = document.getElementById("feedback").value;
   //tarkistetaan nimen pituus:
-  if (käyttäjäNimi.length < 3) {
+  if (käyttäjäNimi === "") {
+    alert("Täytä nimikenttä!");
+    return false;
+  } else if (käyttäjäNimi.length < 3) {
     //console.log("Nimen pituus: " + userName.length + " kirjainta."); //for debugging
     alert("Nimi alle 3 kirjainta!");
     return false;
   }
   //tarkistetaan salasanan pituus (pitää olla yli 15 merkkiä), mitä pidempi sen parempi :D
-  if (salasana.length < 15) {
+  if (salasana === "") {
+    alert("Täytä salasanakenttä!");
+    return false;
+  } else if (salasana.length < 15) {
     //console.log("Salasanan pituus: " + password.length + " merkkiä."); //for debugging
     alert("Liian lyhyt salasana! (alle 15-merkkiä)");
     return false;
