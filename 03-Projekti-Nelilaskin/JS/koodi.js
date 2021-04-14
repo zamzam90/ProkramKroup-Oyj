bc11a887f5385652da18f6e2ef9645374ebd8da6
zamzam90 <siltanen.sami@hotmail.com>
2021-04-14 11:19:10 +0300

    lisää-, laske- ja tyhjennäfunktiot tehty / Sami & Johanna / 14042021

--- 03-Projekti-Nelilaskin/JS/koodi.js
+++ 03-Projekti-Nelilaskin/JS/koodi.js
@@ -1,5 +1,28 @@
 //JS by Johanna & Sami
-//lisää funktio
-//laske funktio
-//tyhjää funktio
-//poista viimeisin funktio?
+//alustetaan muuttujia:
+var tulos = "",
+  luvut = "",
+  operaattorit = ["+", "-", "*", "/", "%"],
+  tyhjätty = "Suorita lasku..";
+//Lisäys-funktio (lisää luvun / operaattorin) lisätään luku tulosmuuttujaan
+function lisää(luvut) {
+  tulos += luvut;
+  console.log("Luku / Operaattori " + luvut + " lisätty laskuun"); //for debugging..
+  console.log(tulos);
+  document.getElementById("näyttö").innerHTML = tulos;
+}
+//lasku-funktio (tässä lasketaan lisätyt luvut eval funktion avulla)
+function laske() {
+  tulos = eval(tulos);
+  console.log("Tulos: " + tulos);
+  document.getElementById("näyttö").innerHTML = tulos;
+}
+/* ToDo..
+//Poisto-funktio (poistaa viimeksi lisätyn luvun & operaattorin)
+*/
+//Alustus-funktio (tyhjentää "näytön")
+function tyhjennä() {
+  console.log("Näyttö tyhjennetty."); //for debugging..
+  tulos = "";
+  document.getElementById("näyttö").innerHTML = tyhjätty;
+}
