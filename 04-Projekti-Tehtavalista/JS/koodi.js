f862651169915286cfb8d6a8d71648028db72114
zamzam90 <siltanen.sami@hotmail.com>
2021-04-20 11:10:17 +0300

    koodia koodia / Sami / 20042021

--- 04-Projekti-Tehtavalista/JS/koodi.js
+++ 04-Projekti-Tehtavalista/JS/koodi.js
@@ -1,1 +1,62 @@
+//Alustetaan muuttujia ja haetaan niihin arvot,
+//Haetaan htmlstä syöttökenttä tehtävälle
+var input = document.getElementById("input");
+//Haetaan htmlstä "lisää tehtävä" painike
+var enterButton = document.getElementById("button");
+//Haetaan documentin ensimmäinen ul elementti, johon li elementit(tehtävät) lisätään
+var ul = document.querySelector("ul");
 
+//Tehdään funktio joka laskee inputtiin syötetyn merkkijonon pituuden
+function inputLength() {
+  return input.value.length; //palautetaan merkkijonon pituus
+}
+
+//Tehdään pääfunktio, joka luo lista elementin(tehtävän)
+function createListElement() {
+  var li = document.createElement("li"); // luodaan li elementti
+  li.appendChild(document.createTextNode(input.value)); //laitetaan syöttökentän arvo(=teksti) li elementin tekstiksi
+  ul.appendChild(li); //lisätään li elementti ul:ään
+  input.value = ""; //tyhjennetään syöttökenttä
+
+  //Valmiin tehtävän värjäys alkaa
+  //Koska tämä on funktion sisällä, koskee vain pääfunktiolla tehtyjä li elementtejä(tehtäviä)
+  function crossOut() {
+    li.classList.toggle("done"); //vaihdetaan li elementin classin done:ksi
+  }
+
+  li.addEventListener("click", crossOut); //kun li elementtiä klikataan, se merkitään tehdyksi crossOut() funktiolla
+  //värjäys päättyy
+
+  //Poista napin lisäys alkaa
+  var dBtn = document.createElement("button"); //luodaan poisto nappi
+  dBtn.appendChild(document.createTextNode("X")); //lisätään nappiin teksti "X"
+  li.appendChild(dBtn); //lisätään nappi li elementtiin
+  dBtn.addEventListener("click", deleteListItem); //kun X:ää painetaan, li elementti poistetaan
+  //Poista napin lisäys päättyy
+
+  //Poisto funktio
+  function deleteListItem() {
+    li.classList.add("delete"); //vaihtaa li elementin classin delete:ksi
+  }
+}
+
+//Funktiot ja eventlistenerit jotka kutsuvat pääfunktiota,
+//Funktio joka kutsuu pääfunktiota napin klikkauksen jälkeen
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    //varmistetaan että syöttökenttään on syötetty jotain
+    createListElement();
+  }
+}
+//Funktio joka kutsuu pääfunktiota enter painalluksen jälkeen
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.which === 13) {
+    //varmistetaan että syöttökenttään on syötetty jotain ja "kuunnellaan" onko enteriä painettu
+    //nro 13 on Enterin avainkoodi, voidaan kirjoittaa myös event.keyCode === 13
+    createListElement();
+  }
+}
+//jos nappia painetaan
+enterButton.addEventListener("click", addListAfterClick);
+//jos syöttökentässä painetaan enter näppäintä
+input.addEventListener("keypress", addListAfterKeypress);
