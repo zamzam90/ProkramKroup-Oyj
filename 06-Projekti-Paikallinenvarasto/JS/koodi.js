501bb02de2831230fe815cce4a75c92adaaa5fcc
zamzam90 <siltanen.sami@hotmail.com>
2021-05-07 14:32:55 +0300

    lomake yhristyy js / Sami / 07052021

--- 06-Projekti-Paikallinenvarasto/JS/koodi.js
+++ 06-Projekti-Paikallinenvarasto/JS/koodi.js
@@ -1,28 +1,23 @@
 //Paikallinen varasto js
 //alert("pööööö"); - toimii!
 function tietoLomake() {
-
-//alustetaan muuttujia, tiedot haetaan lomakkeesta
-var etuNimi = document.getElementById("arvoEtun").value;
-var sukuNimi = document.getElementById("arvoSukun").value;
-var osoite = document.getElementById("arvoLahios").value;
-var postiNumero = document.getElementById("arvoPostin").value;
-var postiPaikka = document.getElementById("arvoPostit").value;
-var puhelin = document.getElementById("arvoPuh").value;
-var sahkoPosti = document.getElementById("arvoSposti").value;
-var varasto = document.getElementById("varastoAlue").value;
-
+  alert("test");
+  //alustetaan muuttujia, tiedot haetaan lomakkeesta
+  var etuNimi = document.getElementById("arvoEtun").value;
+  var sukuNimi = document.getElementById("arvoSukun").value;
+  var osoite = document.getElementById("arvoLahios").value;
+  var postiNumero = document.getElementById("arvoPostin").value;
+  var postiPaikka = document.getElementById("arvoPostit").value;
+  var puhelin = document.getElementById("arvoPuh").value;
+  var sahkoPosti = document.getElementById("arvoSposti").value;
+  var varasto = document.getElementById("varastoAlue").value;
 
   //tarkastetaan etunimen pituus
   if (etuNimi === "") {
     alert("Syötä etunimi!");
     return false;
-  }
-    else if (etuNimi.length < 2) {
+  } else if (etuNimi.length < 2) {
     alert("Antamasi etunimi on liian lyhyt!");
     return false;
   }
-
-
-
 }
