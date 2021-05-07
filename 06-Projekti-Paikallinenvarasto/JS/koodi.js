b63efe7b2b9d8804f352d52931e3fc9f4bb8b57f
JohPii <johanna.piiroinen@edu.keuda.fi>
2021-05-07 13:04:55 +0300

    muokkailua ja js-alotus / Johanna / 07052021

--- 06-Projekti-Paikallinenvarasto/JS/koodi.js
+++ 06-Projekti-Paikallinenvarasto/JS/koodi.js
@@ -1,1 +1,28 @@
-//js
+//Paikallinen varasto js
+//alert("pööööö"); - toimii!
+function tietoLomake() {
+
+//alustetaan muuttujia, tiedot haetaan lomakkeesta
+var etuNimi = document.getElementById("arvoEtun").value;
+var sukuNimi = document.getElementById("arvoSukun").value;
+var osoite = document.getElementById("arvoLahios").value;
+var postiNumero = document.getElementById("arvoPostin").value;
+var postiPaikka = document.getElementById("arvoPostit").value;
+var puhelin = document.getElementById("arvoPuh").value;
+var sahkoPosti = document.getElementById("arvoSposti").value;
+var varasto = document.getElementById("varastoAlue").value;
+
+
+  //tarkastetaan etunimen pituus
+  if (etuNimi === "") {
+    alert("Syötä etunimi!");
+    return false;
+  }
+    else if (etuNimi.length < 2) {
+    alert("Antamasi etunimi on liian lyhyt!");
+    return false;
+  }
+
+
+
+}
