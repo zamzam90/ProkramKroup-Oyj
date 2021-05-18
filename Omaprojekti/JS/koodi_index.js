57c9094004d9a452ffa4d00f58db500f016e1fee
zamzam90 <siltanen.sami@hotmail.com>
2021-05-18 11:26:58 +0300

    etusivun slideshowta hifistelty, kesken.. / Sami / 18052021

--- Omaprojekti/JS/koodi_index.js
+++ Omaprojekti/JS/koodi_index.js
@@ -19,3 +19,34 @@ function slideShow() {
   setTimeout(slideShow, 2000); // Change image every 2 seconds
 }
 /* slideshow stuff end */
+
+/* modal image stuff */
+var slideShowContainer = document.querySelector(".slideShow-container");
+var slideShowContainerImages =
+  slideShowContainer.querySelectorAll("div.slide > img");
+for (k = 0; k < slideShowContainerImages.length; k++) {
+  slideShowContainerImages[k].addEventListener("click", openModal);
+}
+
+function openModal() {
+  // console.log(this.src);
+  // Get the modal
+  var modal = document.getElementById("modal");
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  // var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("modal-image");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML =
+    this.alt + "<br><a href='./esittely.html'>Varaa matka!</a>";
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("modal-close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+}
+/* modal image stuff end */
